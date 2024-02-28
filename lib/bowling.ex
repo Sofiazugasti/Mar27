61b80ba4cea15bdfe2efbb967f47fedc8f965f82
Sofia Zugasti <sofiazugasti04@gmail.com>
2024-02-28 00:13:55 -0600

    test 5

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -4,5 +4,8 @@ defmodule Bowling do
   end
   def score([[a, b | _] | others]) when a == 10, do: a + hd(hd(others)) + hd(tl(hd(others))) + score(others)
   def score([[a, b | _] | others]) when a + b == 10, do: a + b + hd(hd(others)) + score(others)
+  def score([[10, nil] | others]) do
+    10 + hd(hd(others)) + hd(tl(hd(others))) + score(others)
+  end
   def score([[a, b | _] | others]), do: a + b + score(others)
 end
