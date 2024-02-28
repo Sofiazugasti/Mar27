4563636d744246f9da72ea5cd62a641720772aed
Sofia Zugasti <sofiazugasti04@gmail.com>
2024-02-27 23:59:21 -0600

    Initial commit -- Bowling

---
+++ lib/bowling.ex
@@ -0,0 +1,8 @@
+defmodule Bowling do
+  def score([])do
+    0
+  end
+  def score([[a, b | _] | others]) when a == 10, do: a + hd(hd(others)) + hd(tl(hd(others))) + score(others)
+  def score([[a, b | _] | others]) when a + b == 10, do: a + b + hd(hd(others)) + score(others)
+  def score([[a, b | _] | others]), do: a + b + score(others)
+end
